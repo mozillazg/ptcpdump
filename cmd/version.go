eee278dfdcd04d9e1db34368b33c05774e9262d5
mozillazg <mozillazg101@gmail.com>
2024-04-27 10:16:11 +0800

    strip `v` when output version

--- cmd/version.go
+++ cmd/version.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"github.com/gopacket/gopacket/pcap"
 	"github.com/mozillazg/ptcpdump/internal"
+	"strings"
 )
 
 func printVersion() error {
-	fmt.Printf("ptcpdump version %s\n", internal.Version)
+	fmt.Printf("ptcpdump version %s\n", strings.TrimLeft(internal.Version, "v"))
 	fmt.Printf("%s\n", pcap.Version())
 	return nil
 }
