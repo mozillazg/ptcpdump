07835c94bd54d9099d0407dacb98a4e13538a5f8
mozillazg <mozillazg101@gmail.com>
2025-01-25 05:40:45 +0000

    fix running on linux 4.19 ~ 5.7 failed

--- bpf/bpf.go
+++ bpf/bpf.go
@@ -144,6 +144,7 @@ load:
 		IgnoreUnknownProgram:      true,
 		IgnoreNotSupportedProgram: true,
 		IgnoreUnknownVariable:     true,
+		IgnoreInvalidMap:          true,
 	})
 	if err != nil {
 		log.Infof("load and assign failed: %+v", err)
