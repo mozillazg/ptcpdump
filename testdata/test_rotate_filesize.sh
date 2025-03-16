49fe004046e4f6f5f65c840c7118b4bc7f61cd8d
mozillazg <mozillazg101@gmail.com>
2025-03-16 12:39:18 +0000

    chore(ci): running docker/containerd/k8s tests on github action ci

--- testdata/test_rotate_filesize.sh
+++ testdata/test_rotate_filesize.sh
@@ -10,9 +10,9 @@ function test_ptcpdump() {
   ext="$1"
   FNAME="${FILE_PREFIX}_rotate_c_$(date +%s).${ext}"
 
-  timeout 30s ${CMD} -C 1kb ${PTCPDUMP_EXTRA_ARGS} -i any -w "${FNAME}" \
+  timeout 60s ${CMD} -C 1kb ${PTCPDUMP_EXTRA_ARGS} -i any -w "${FNAME}" \
     'port 80 and host lax-ca-us-ping.vultr.com' &
-  curl -m 10 http://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin &>/dev/null || true
+  curl -m 30 --retry 2 --retry-all-errors http://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin &>/dev/null || true
   wait
   ls -lh ${FNAME}*
   test $(ls ${FNAME}* | wc -l) -gt 3
--- testdata/test_rotate_filesize_with_count.sh
+++ testdata/test_rotate_filesize_with_count.sh
@@ -10,9 +10,9 @@ function test_ptcpdump() {
   ext="$1"
   FNAME="${FILE_PREFIX}_rotate_cw_$(date +%s).${ext}"
 
-  timeout 30s ${CMD} -C 1kb -W 3 ${PTCPDUMP_EXTRA_ARGS} -i any -w "${FNAME}" \
+  timeout 60s ${CMD} -C 1kb -W 3 ${PTCPDUMP_EXTRA_ARGS} -i any -w "${FNAME}" \
     'port 80 and host lax-ca-us-ping.vultr.com' &
-  curl -m 10 http://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin &>/dev/null || true
+  curl -m 30 --retry 2 --retry-all-errors http://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin &>/dev/null || true
   wait
   ls -lh ${FNAME}*
   test $(ls ${FNAME}* | wc -l) == 3
