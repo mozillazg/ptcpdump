aa1b107de51d99bc75f1f1dd17ec71418f950393
mozillazg <mozillazg101@gmail.com>
2025-10-01 07:36:23 +0000

    refactor: dedupe pcap filter injection helpers

--- bpf/pcap_filter.go
+++ bpf/pcap_filter.go
@@ -2,14 +2,65 @@ package bpf
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/cilium/ebpf"
 	"github.com/jschwinger233/elibpcap"
 	"github.com/mozillazg/ptcpdump/internal/log"
 	"github.com/mozillazg/ptcpdump/internal/types"
-	"strings"
 )
 
+type pcapFilterCase struct {
+	enabled bool
+	opts    elibpcap.Options
+}
+
+func (b *BPF) programSpec(progName string) (*ebpf.ProgramSpec, bool) {
+	prog, ok := b.spec.Programs[progName]
+	if !ok {
+		log.Infof("program %s not found", progName)
+		return nil, false
+	}
+	if prog == nil {
+		log.Infof("program %s is nil", progName)
+		return nil, false
+	}
+	return prog, true
+}
+
+func (b *BPF) injectProgramFilters(progName string, prog *ebpf.ProgramSpec, cases ...pcapFilterCase) error {
+	for _, c := range cases {
+		if !c.enabled {
+			continue
+		}
+		if err := injectWithFallback(prog, b.opts.pcapFilter, c.opts); err != nil {
+			return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
+		}
+	}
+	return nil
+}
+
+func injectWithFallback(prog *ebpf.ProgramSpec, filter string, opts elibpcap.Options) error {
+	original := prog.Instructions
+	ins, err := elibpcap.Inject(filter, original, opts)
+	if err != nil {
+		if !isRejectAllPacketsErr(err) {
+			return err
+		}
+		ins, err = elibpcap.Inject(elibpcap.RejectAllExpr, original, opts)
+		if err != nil {
+			return err
+		}
+	}
+	prog.Instructions = ins
+	return nil
+}
+
+func isRejectAllPacketsErr(err error) bool {
+	return strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets")
+}
+
 func (b *BPF) injectPcapFilterToTcs() error {
-	var err error
 	for _, progName := range []string{
 		"ptcpdump_tc_ingress_l2", "ptcpdump_tc_ingress_l3",
 		"ptcpdump_tc_egress_l2", "ptcpdump_tc_egress_l3",
@@ -18,13 +69,8 @@ func (b *BPF) injectPcapFilterToTcs() error {
 		"ptcpdump_cgroup_skb__ingress",
 		"ptcpdump_cgroup_skb__egress",
 	} {
-		prog, ok := b.spec.Programs[progName]
+		prog, ok := b.programSpec(progName)
 		if !ok {
-			log.Infof("program %s not found", progName)
-			continue
-		}
-		if prog == nil {
-			log.Infof("program %s is nil", progName)
 			continue
 		}
 		l2skb := true
@@ -38,209 +84,101 @@ func (b *BPF) injectPcapFilterToTcs() error {
 		}
 
 		log.Infof("inject pcap filter to %s", progName)
-		if strings.HasSuffix(progName, "l2") || l2skb {
-			prog.Instructions, err = elibpcap.Inject(
-				b.opts.pcapFilter,
-				prog.Instructions,
-				elibpcap.Options{
+		if err := b.injectProgramFilters(
+			progName,
+			prog,
+			pcapFilterCase{
+				enabled: strings.HasSuffix(progName, "l2") || l2skb,
+				opts: elibpcap.Options{
 					AtBpf2Bpf:        "pcap_filter",
 					PacketAccessMode: elibpcap.Direct,
 					L2Skb:            true,
 				},
-			)
-			if err != nil {
-				if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-					prog.Instructions, err = elibpcap.Inject(
-						elibpcap.RejectAllExpr,
-						prog.Instructions,
-						elibpcap.Options{
-							AtBpf2Bpf:        "pcap_filter",
-							PacketAccessMode: elibpcap.Direct,
-							L2Skb:            true,
-						},
-					)
-				}
-			}
-			if err != nil {
-				return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
-			}
-		}
-
-		if strings.HasSuffix(progName, "l3") || !l2skb {
-			prog.Instructions, err = elibpcap.Inject(
-				b.opts.pcapFilter,
-				prog.Instructions,
-				elibpcap.Options{
+			},
+			pcapFilterCase{
+				enabled: strings.HasSuffix(progName, "l3") || !l2skb,
+				opts: elibpcap.Options{
 					AtBpf2Bpf:        "pcap_filter_l3",
 					PacketAccessMode: elibpcap.Direct,
 					L2Skb:            false,
 				},
-			)
-			if err != nil {
-				if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-					prog.Instructions, err = elibpcap.Inject(
-						elibpcap.RejectAllExpr,
-						prog.Instructions,
-						elibpcap.Options{
-							AtBpf2Bpf:        "pcap_filter_l3",
-							PacketAccessMode: elibpcap.Direct,
-							L2Skb:            false,
-						},
-					)
-				}
-			}
-			if err != nil {
-				return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
-			}
+			},
+		); err != nil {
+			return err
 		}
 	}
-	return err
+	return nil
 }
 
 func (b *BPF) injectPcapFilterToTpBtfs() error {
-	var err error
 	for _, progName := range []string{
 		"ptcpdump_tp_btf__net_dev_queue",
 		"ptcpdump_tp_btf__netif_receive_skb",
 	} {
-		prog, ok := b.spec.Programs[progName]
+		prog, ok := b.programSpec(progName)
 		if !ok {
-			log.Infof("program %s not found", progName)
-			continue
-		}
-		if prog == nil {
-			log.Infof("program %s is nil", progName)
 			continue
 		}
 
 		log.Infof("inject pcap filter to %s", progName)
-		prog.Instructions, err = elibpcap.Inject(
-			b.opts.pcapFilter,
-			prog.Instructions,
-			elibpcap.Options{
-				AtBpf2Bpf:        "pcap_filter",
-				PacketAccessMode: elibpcap.BpfProbeReadKernel,
-				L2Skb:            true,
+		if err := b.injectProgramFilters(
+			progName,
+			prog,
+			pcapFilterCase{
+				enabled: true,
+				opts: elibpcap.Options{
+					AtBpf2Bpf:        "pcap_filter",
+					PacketAccessMode: elibpcap.BpfProbeReadKernel,
+					L2Skb:            true,
+				},
 			},
-		)
-		if err != nil {
-			if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-				prog.Instructions, err = elibpcap.Inject(
-					elibpcap.RejectAllExpr,
-					prog.Instructions,
-					elibpcap.Options{
-						AtBpf2Bpf:        "pcap_filter",
-						PacketAccessMode: elibpcap.BpfProbeReadKernel,
-						L2Skb:            true,
-					},
-				)
-			}
-		}
-		if err != nil {
-			return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
-		}
-
-		prog.Instructions, err = elibpcap.Inject(
-			b.opts.pcapFilter,
-			prog.Instructions,
-			elibpcap.Options{
-				AtBpf2Bpf:        "pcap_filter_l3",
-				PacketAccessMode: elibpcap.BpfProbeReadKernel,
-				L2Skb:            false,
+			pcapFilterCase{
+				enabled: true,
+				opts: elibpcap.Options{
+					AtBpf2Bpf:        "pcap_filter_l3",
+					PacketAccessMode: elibpcap.BpfProbeReadKernel,
+					L2Skb:            false,
+				},
 			},
-		)
-		if err != nil {
-			if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-				prog.Instructions, err = elibpcap.Inject(
-					elibpcap.RejectAllExpr,
-					prog.Instructions,
-					elibpcap.Options{
-						AtBpf2Bpf:        "pcap_filter_l3",
-						PacketAccessMode: elibpcap.BpfProbeReadKernel,
-						L2Skb:            false,
-					},
-				)
-			}
-		}
-		if err != nil {
-			return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
+		); err != nil {
+			return err
 		}
 	}
-	return err
+	return nil
 }
 
 func (b *BPF) injectPcapFilterToSocketFilters() error {
-	var err error
-
 	for _, progName := range []string{
 		"ptcpdump_socket_filter__ingress_l2", "ptcpdump_socket_filter__ingress_l3",
 		"ptcpdump_socket_filter__egress_l2", "ptcpdump_socket_filter__egress_l3",
 	} {
-		prog, ok := b.spec.Programs[progName]
+		prog, ok := b.programSpec(progName)
 		if !ok {
-			log.Infof("program %s not found", progName)
-			continue
-		}
-		if prog == nil {
-			log.Infof("program %s is nil", progName)
 			continue
 		}
 
 		log.Infof("inject pcap filter to %s", progName)
-		if strings.HasSuffix(progName, "l2") {
-			prog.Instructions, err = elibpcap.Inject(
-				b.opts.pcapFilter,
-				prog.Instructions,
-				elibpcap.Options{
+		if err := b.injectProgramFilters(
+			progName,
+			prog,
+			pcapFilterCase{
+				enabled: strings.HasSuffix(progName, "l2"),
+				opts: elibpcap.Options{
 					AtBpf2Bpf:        "pcap_filter",
 					PacketAccessMode: elibpcap.BpfSkbLoadBytes,
 					L2Skb:            true,
 				},
-			)
-			if err != nil {
-				if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-					prog.Instructions, err = elibpcap.Inject(
-						elibpcap.RejectAllExpr,
-						prog.Instructions,
-						elibpcap.Options{
-							AtBpf2Bpf:        "pcap_filter",
-							PacketAccessMode: elibpcap.BpfSkbLoadBytes,
-							L2Skb:            true,
-						},
-					)
-				}
-			}
-			if err != nil {
-				return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
-			}
-		}
-
-		if strings.HasSuffix(progName, "l3") {
-			prog.Instructions, err = elibpcap.Inject(
-				b.opts.pcapFilter,
-				prog.Instructions,
-				elibpcap.Options{
+			},
+			pcapFilterCase{
+				enabled: strings.HasSuffix(progName, "l3"),
+				opts: elibpcap.Options{
 					AtBpf2Bpf:        "pcap_filter_l3",
 					PacketAccessMode: elibpcap.BpfSkbLoadBytes,
 					L2Skb:            false,
 				},
-			)
-			if err != nil {
-				if strings.Contains(fmt.Sprintf("%+v", err), "expression rejects all packets") {
-					prog.Instructions, err = elibpcap.Inject(
-						elibpcap.RejectAllExpr,
-						prog.Instructions,
-						elibpcap.Options{
-							AtBpf2Bpf:        "pcap_filter_l3",
-							PacketAccessMode: elibpcap.BpfSkbLoadBytes,
-							L2Skb:            false,
-						},
-					)
-				}
-			}
-			if err != nil {
-				return fmt.Errorf("inject pcap filter to %s: %w", progName, err)
-			}
+			},
+		); err != nil {
+			return err
 		}
 	}
 
